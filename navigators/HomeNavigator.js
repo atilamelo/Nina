3a6aa5fee167211696d136454793868ba57d315d
Átila <atilamelodesouza@gmail.com>
2023-08-20 01:58:19 -0300

    Refatoração HomeNavigator

--- navigators/HomeNavigator.js
+++ navigators/HomeNavigator.js
@@ -1,153 +1,156 @@
-/* Creator: Átila Melo
- * Data: 28/06/2023
- * Description: Create the navigation bar of the app
- * Credits: www.youtube.com/watch?v=gPaBicMaib4
- */
-
+import React from 'react';
+import { Image, TouchableOpacity, View, Text } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { StyleSheet, Text, View, Image, Touchable, TouchableOpacity } from 'react-native';
-
-import HomeScreen from '../screens/HomeScreen';
-import StatisticsScreen from '../screens/StatisticsScreen';
-import CalendarScreen from '../screens/CalendarScreen';
-import ConfigScreen from '../screens/ConfigScreen';
-import WriteNavigator from './WriteNavigator';
-import ConfigNavigator from './ConfigNavigator';
+import HomeScreen from '@screens/HomeScreen';
+import StatisticsScreen from '@screens/StatisticsScreen';
+import CalendarScreen from '@screens/CalendarScreen';
+import WriteNavigator from '@navigators/WriteNavigator';
+import ConfigNavigator from '@navigators/ConfigNavigator';
 
 const Tab = createBottomTabNavigator();
+
 const focusedColor = '#9F238E';
 const unfocusedColor = '#585C6D';
 
-const AddDreamButton = ({ children, onPress }) => {
-    return (
-        <TouchableOpacity
-            style={{
-                top: -25,
-                justifyContent: 'center',
-                alignItems: 'center',
-            }}
-            onPress={onPress}
-        >
-            <View style={{
-                width: 70,
-                height: 70,
-                borderRadius: 35,
-                backgroundColor: '#653483'
-            }}>
-                {children}
-            </View>
-        </TouchableOpacity>
-    );
-}
+const CustomTabBarButton = ({ children, onPress }) => (
+  <TouchableOpacity
+    style={{
+      top: -25,
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}
+    onPress={onPress}
+  >
+    <View
+      style={{
+        width: 70,
+        height: 70,
+        borderRadius: 35,
+        backgroundColor: '#653483',
+      }}
+    >
+      {children}
+    </View>
+  </TouchableOpacity>
+);
 
-
-const Tabs = () => {
-    return (
-        <Tab.Navigator
-            screenOptions={{
-                tabBarShowLabel: false,
-                tabBarStyle: {
-                    position: 'absolute',
-                    height: 60,
-                    backgroundColor: '#222840',
-                    borderTopWidth: 0,
-                    borderTopLeftRadius: 5,
-                    borderTopRightRadius: 5,
-                },
-                tabBarInactiveTintColor: unfocusedColor,
-                tabBarActiveTintColor: focusedColor,
-                headerShown: false,
-
-            }}
-            initialRouteName='Home'
-        >
-            <Tab.Screen name="Home" component={HomeScreen} options={{
-                tabBarIcon: ({ focused }) => (
-                    <View style={styles.tabIconContainer}>
-                        <Image
-                            source={require('@assets/icons/home.png')}
-                            resizeMode="contain"
-                            style={[styles.tabIcon, { tintColor: focused ? focusedColor : unfocusedColor }]}
-                        />
-                        <Text style={[styles.tabText, { color: focused ? focusedColor : unfocusedColor }]}>Home</Text>
-                    </View>
-                ),
-            }} />
-
-            <Tab.Screen name="Estatistícas" component={StatisticsScreen} options={{
-                tabBarIcon: ({ focused }) => (
-                    <View style={styles.tabIconContainer}>
-                        <Image
-                            source={require('@assets/icons/statistics.png')}
-                            resizeMode="contain"
-                            style={[styles.tabIcon, { tintColor: focused ? focusedColor : unfocusedColor }]}
-                        />
-                        <Text style={[styles.tabText, { color: focused ? focusedColor : unfocusedColor }]}>Estatistícas</Text>
-                    </View>
-                ),
-            }} />
-
-            <Tab.Screen name="WriteDreams" component={WriteNavigator} options={{
-                tabBarIcon: ({ focused }) => (
-                    <Image
-                        source={require('@assets/icons/pen.png')}
-                        resizeMode="contain"
-                        style={{
-                            width: 30,
-                            height: 30,
-                            tintColor: '#fff'
-                        }}
-                    />
-                ),
-                tabBarButton: ( props ) => (
-                    <AddDreamButton  { ... props}/>
-                ),
-                tabBarStyle: {display: 'none'}
-            }}/>
-
-            <Tab.Screen name="Calendário" component={CalendarScreen} options={{
-                tabBarIcon: ({ focused }) => (
-                    <View style={styles.tabIconContainer}>
-                        <Image
-                            source={require('@assets/icons/calendar.png')}
-                            resizeMode="contain"
-                            style={[styles.tabIcon, { tintColor: focused ? focusedColor : unfocusedColor }]}
-                        />
-                        <Text style={[styles.tabText, { color: focused ? focusedColor : unfocusedColor }]}>Calendário</Text>
-                    </View>
-                ),
-            }} />
-            <Tab.Screen name="Configurações" component={ConfigNavigator} options={{
-                tabBarIcon: ({ focused }) => (
-                    <View style={styles.tabIconContainer}>
-                        <Image
-                            source={require('@assets/icons/settings.png')}
-                            resizeMode="contain"
-                            style={[styles.tabIcon, { tintColor: focused ? focusedColor : unfocusedColor }]}
-                        />
-                        <Text style={[styles.tabText, { color: focused ? focusedColor : unfocusedColor }]}>Config.</Text>
-                    </View>
-                ),
-                
-            }} />
-        </Tab.Navigator>
-    );
-}
-
-const styles = StyleSheet.create({
-    tabIconContainer: {
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    tabIcon: {
+const TabIcon = ({ focused, source, text }) => (
+  <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+    <Image
+      source={source}
+      resizeMode="contain"
+      style={{
         width: 23,
         height: 23,
-    },
-    tabText: {
+        tintColor: focused ? focusedColor : unfocusedColor,
+      }}
+    />
+    <Text
+      style={{
         paddingTop: 5,
         fontSize: 11,
-    },
-});
+        color: focused ? focusedColor : unfocusedColor,
+      }}
+    >
+      {text}
+    </Text>
+  </View>
+);
 
+const HomeTab = () => (
+  <Tab.Navigator
+    screenOptions={{
+      tabBarShowLabel: false,
+      tabBarStyle: {
+        position: 'absolute',
+        height: 60,
+        backgroundColor: '#222840',
+        borderTopWidth: 0,
+        borderTopLeftRadius: 5,
+        borderTopRightRadius: 5,
+      },
+      tabBarInactiveTintColor: unfocusedColor,
+      tabBarActiveTintColor: focusedColor,
+      headerShown: false,
+    }}
+    initialRouteName="Home"
+  >
+    <Tab.Screen
+      name="Home"
+      component={HomeScreen}
+      options={{
+        tabBarIcon: ({ focused }) => (
+          <TabIcon
+            focused={focused}
+            source={require('@assets/icons/home.png')}
+            text="Home"
+          />
+        ),
+      }}
+    />
 
-export default Tabs;
+    <Tab.Screen
+      name="Statistics"
+      component={StatisticsScreen}
+      options={{
+        tabBarIcon: ({ focused }) => (
+          <TabIcon
+            focused={focused}
+            source={require('@assets/icons/statistics.png')}
+            text="Statistics"
+          />
+        ),
+      }}
+    />
+
+    <Tab.Screen
+      name="WriteDreams"
+      component={WriteNavigator}
+      options={{
+        tabBarIcon: ({ focused }) => (
+          <Image
+            source={require('@assets/icons/pen.png')}
+            resizeMode="contain"
+            style={{
+              width: 30,
+              height: 30,
+              tintColor: '#fff',
+            }}
+          />
+        ),
+        tabBarButton: (props) => <CustomTabBarButton {...props} />,
+        tabBarStyle: { display: 'none' },
+      }}
+    />
+
+    <Tab.Screen
+      name="Calendar"
+      component={CalendarScreen}
+      options={{
+        tabBarIcon: ({ focused }) => (
+          <TabIcon
+            focused={focused}
+            source={require('@assets/icons/calendar.png')}
+            text="Calendar"
+          />
+        ),
+      }}
+    />
+
+    <Tab.Screen
+      name="Settings"
+      component={ConfigNavigator}
+      options={{
+        tabBarIcon: ({ focused }) => (
+          <TabIcon
+            focused={focused}
+            source={require('@assets/icons/settings.png')}
+            text="Settings"
+          />
+        ),
+      }}
+    />
+  </Tab.Navigator>
+);
+
+export default HomeTab;
