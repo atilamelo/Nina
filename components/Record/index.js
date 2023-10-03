f2d3cf830e96ebc03993faf0ec045dce9bdc3065
CarlaChicareli <20212014267@aluno.cefetmg.br>
2023-10-03 17:39:09 -0300

    Implementado a gravação de audio
    --> Ainda não é possivel adicionar o audio a escrita de sonhos
    
    --> Ainda não é possivel voltar após clicar no audio
    
    --> Ainda devo fazer com que não seja necessario o usuario clicar o tempo inteiro no audio para envialo

--- components/Record/index.js
+++ components/Record/index.js
@@ -1,29 +1,114 @@
-import React from 'react';
-import {TouchableOpacity, Image } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { Pressable, Image, Alert, TouchableOpacity } from 'react-native';
 import styled from 'styled-components/native';
-import UpperDescription from '@components/Record/UpperDescription';
+import { Audio, InterruptionModeAndroid, InterruptionModeIOS } from 'expo-av';
 
-const Record = (onPress) => {
+const Record = () => {
+  const [recording, setRecording] = useState();
+  const [recordingFileURI, setRecordingFileURI] = useState();
+  const [timer, setTimer] = useState(0);
+  const timerRef = useRef();
+
+  const startTimer = () => {
+    timerRef.current = setInterval(() => {
+      setTimer((prevTimer) => prevTimer + 1);
+    }, 1000);
+  };
+
+  const stopTimer = () => {
+    clearInterval(timerRef.current);
+    setTimer(0);
+  };
+
+  const formatTime = (timeInSeconds) => {
+    const minutes = Math.floor(timeInSeconds / 60);
+    const seconds = timeInSeconds % 60;
+    return `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+  };
+
+  async function handleRecordingStart() {
+    const { granted } = await Audio.getPermissionsAsync();
+
+    if (granted) {
+      try {
+        const { recording } = await Audio.Recording.createAsync();
+        setRecording(recording);
+        startTimer();
+      } catch (error) {
+        console.log('error', error);
+        Alert.alert('Erro ao gravar', 'Não foi possível iniciar a gravação do áudio.');
+      }
+    }
+  }
+
+  async function handleRecordingStop() {
+    try {
+      if (recording) {
+        await recording.stopAndUnloadAsync();
+        const fileUri = recording.getURI();
+
+        console.log(fileUri);
+
+        setRecordingFileURI(fileUri);
+        setRecording(null);
+        stopTimer();
+      }
+    } catch (error) {
+      console.log('error', error);
+      Alert.alert('Erro ao pausar', 'Não foi possível parar a gravação do áudio.');
+    }
+  }
+
+  useEffect(() => {
+    Audio.requestPermissionsAsync().then(({ granted }) => {
+      if (granted) {
+        Audio.setAudioModeAsync({
+          allowsRecordingIOS: true,
+          interruptionModeIOS: InterruptionModeIOS.DoNotMix,
+          playsInSilentModeIOS: true,
+          allowsRecordingAndroid: true,
+          interruptionModeAndroid: InterruptionModeAndroid.DoNotMix,
+          shouldDuckAndroid: true,
+          playThroughEarpieceAndroid: true,
+        });
+      }
+    });
+
+    return () => {
+      if (timerRef.current) {
+        clearInterval(timerRef.current);
+      }
+    };
+  }, []);
+
   return (
     <>
-        <LowerRectangleContainer>
-        <TouchableEllipse>
-            <Image source={require('@assets/icons/micPreenchido.png')} style={{ width: 21, height: 30, tintColor: 'white' }} />
-        </TouchableEllipse>
-        <Texto>Segure o Microfone para falar</Texto>
-        </LowerRectangleContainer>
+      <LowerRectangleContainer>
+        <PressableEllipse
+          recording={recording}
+          onPressIn={handleRecordingStart}
+          onPressOut={handleRecordingStop}>
+          <Image source={recording ? require('@assets/icons/rec.png') : require('@assets/icons/micPreenchido.png')} style={{ width: recording ? 25 : 21, height: recording ? 25 : 30 }} />
+        </PressableEllipse>
+        {recording ? <Tempo>{`${formatTime(timer)}`}</Tempo> : <Texto>Segure o Microfone para falar</Texto>}
+      </LowerRectangleContainer>
 
-        <UpperRectangleContainer>
-            <UpperDescription/>
-        </UpperRectangleContainer>
+      <UpperRectangleContainer>
+        <Content>
+            <TouchableOpacity onPress={() => {}}>
+                <Image source={require('@assets/icons/Voltar.png')} style={{ width: 14, height: 24 }} />
+            </TouchableOpacity>
+            <Descricao>Descreva seu sonho</Descricao>
+        </Content>
+      </UpperRectangleContainer>
     </>
   );
 };
 
-const TouchableEllipse = styled.TouchableOpacity`
+const PressableEllipse = styled.Pressable`
   width: 60px;
   height: 60px;
-  background-color: #9F238E;
+  background-color: ${({ recording }) => (recording ? 'red' : '#9F238E')};
   border-radius: 30px;
   align-self: center;
   margin-top: 65px;
@@ -50,10 +135,17 @@ const LowerRectangleContainer = styled.View`
 `;
 
 const Texto = styled.Text`
-    color: #D9D9D9;
-    text-align: center;
-    font-family: "Inter Regular";
-    font-size: 13px;
+  color: #d9d9d9;
+  text-align: center;
+  font-family: 'Inter Regular';
+  font-size: 13px;
+`;
+
+const Tempo = styled.Text`
+  color: #fff;
+  text-align: center;
+  font-family: 'Inter Regular';
+  font-size: 20px;
 `;
 
 const Descricao = styled.Text`
--- screens/WriteScreen/index.js
+++ screens/WriteScreen/index.js
@@ -7,111 +7,103 @@ import BackHeader from '@components/Headers/BackHeader';
 import Background from '@components/Background';
 import styled from 'styled-components/native';
 
-const WriteScreen = ( { navigation } ) => {
+import Record from '@components/Record';
+
+const WriteScreen = ({ navigation }) => {
     const dreamContext = useContext(DreamContext);
     const dreamData = dreamContext.dreamData;
     const setDreamData = dreamContext.setDreamData;
   
+    const [showRecord, setShowRecord] = useState(false);
+  
     const voltarButton = () => {
-        navigation.navigate('Sonhos');
+      navigation.navigate('Sonhos');
     };
-
+  
     const nextScreen = () => {
-        navigation.navigate('DreamRec');
+      navigation.navigate('DreamRec');
     };
-
+  
     const generateImage = () => {
-        navigation.navigate('DreamImage');
+      navigation.navigate('DreamImage');
     };
-
-    const onChangeTitle = ( title ) => {
-        setDreamData(prevData => ({
-            ...prevData,
-            title: title
-        }))
+  
+    const onChangeTitle = (title) => {
+      setDreamData((prevData) => ({
+        ...prevData,
+        title: title,
+      }));
     };
-
-    const onChangeText = ( text ) => {
-        setDreamData(prevData => ({
-            ...prevData,
-            text: text
-        }))
+  
+    const onChangeText = (text) => {
+      setDreamData((prevData) => ({
+        ...prevData,
+        text: text,
+      }));
     };
-
+  
+    const toggleRecord = () => {
+      setShowRecord(!showRecord);
+    };
+  
     return (
-        
-        <KeyboardAvoidingView
-            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-            style={styles.container}
-            windowSoftInputMode="adjustResize"
-        >
-            <Background>
-                
-                <BackHeader onPress={() => {navigation.goBack()}} title={'Escreva seu sonho'}/>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        style={styles.container}
+        windowSoftInputMode="adjustResize"
+      >
+        <Background>
+          <BackHeader onPress={() => navigation.goBack()} title={'Escreva seu sonho'} />
+  
+          <ScrollView contentContainerStyle={{ paddingBottom: 120 }}>
+            <Container>
+              <Titulo
+                placeholder="Título"
+                placeholderTextColor="#B4B4B4"
+                type="text"
+                name="titulo"
+                multiline={true}
+                onChangeText={onChangeTitle}
+                selectionColor="purple"
+                maxLength={50}
+                value={dreamData.title}
+              />
+  
+              <>{Titulo.titulo}</>
+  
+              <Sonho
+                placeholder="Escreva aqui seu sonho."
+                placeholderTextColor="#B4B4B4"
+                type="text"
+                name="texto"
+                multiline={true}
+                onChangeText={onChangeText}
+                selectionColor="purple"
+                value={dreamData.text}
+              />
+            </Container>
+          </ScrollView>
+          
+  
+          <DreamFooter style={{ justifyContent: 'space-between' }}>
+            <View style={{ flexDirection: 'row' }}>
+              <View style={{ marginRight: 27 }}>
+                {/* Gravar com o áudio */}
+                <BasicButton onPress={toggleRecord} iconFile={require('@assets/icons/mic.png')} iconWidth={31} iconHeight={29} />
+              </View>
+  
+              {/* Gerar imagem com IA */}
+              <BasicButton onPress={generateImage} iconFile={require('@assets/icons/img.png')} iconWidth={27} iconHeight={22} />
+            </View>
+  
+            <DegradeButton onPress={nextScreen} iconFile={require('@assets/icons/arrow.png')} iconWidth={22} iconHeight={22} />
+          </DreamFooter>
 
-                <ScrollView contentContainerStyle={{ paddingBottom: 120 }}>
-                    <Container>
-                        <Titulo 
-                            placeholder="Título"
-                            placeholderTextColor="#B4B4B4"
-                            type = "text" 
-                            name = "titulo"
-                            multiline={true} // Habilita várias linhas
-                            onChangeText={onChangeTitle}
-                            selectionColor="purple"
-                            maxLength={50}
-                            value={dreamData.title}
-                        />
-
-                        <>{Titulo.titulo}</>
-
-                        <Sonho 
-                            placeholder="Escreva aqui seu sonho."
-                            placeholderTextColor="#B4B4B4"
-                            type="text" 
-                            name="texto"
-                            multiline={true}
-                            onChangeText={onChangeText}
-                            selectionColor="purple"
-                            value={dreamData.text}
-                        />
-
-                    </Container>
-                </ScrollView>
-                <DreamFooter style={{justifyContent: 'space-between'}}>
-                    <View style={{flexDirection: 'row'}}>
-                        <View style={{marginRight: 27}}>
-                            {/* Gravar com o áudio */}
-                            <BasicButton 
-                                onPress={() => undefined}
-                                iconFile={require('@assets/icons/mic.png')}
-                                iconWidth={31}
-                                iconHeight={29}
-                            />
-                        </View>
-                        
-                        {/* Gerar imagem com IA */}
-                        <BasicButton 
-                            onPress={generateImage}
-                            iconFile={require('@assets/icons/img.png')}
-                            iconWidth={27}
-                            iconHeight={22}
-                        />
-                    </View>
-
-
-                    <DegradeButton 
-                        onPress={nextScreen}
-                        iconFile={require('@assets/icons/arrow.png')}
-                        iconWidth={22}
-                        iconHeight={22}
-                    />                
-                </DreamFooter>
-                
-            </Background>
-        </KeyboardAvoidingView>
+          {showRecord && <Record />}
+        </Background>
+      </KeyboardAvoidingView>
     );
-}
+  };
 
 const Container = styled.View`
     flex: 1;
