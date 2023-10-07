cc6d6402198e4f8a144535a345dfcce3b6170c9f
CarlaChicareli <20212014267@aluno.cefetmg.br>
2023-10-06 21:43:04 -0300

    Calendario feito
    --> Falta adicionar os sonhos tidos em cada dia

--- screens/CalendarScreen/index.js
+++ screens/CalendarScreen/index.js
@@ -1,28 +1,102 @@
-/* Creator: Átila Melo
- * Data: 28/06/2023
- * Description: Calendar Screen that will show the registered dreams based on the day selected by the use
- */
-
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, Image } from 'react-native';
 import Background from '@components/Background';
+import styled from 'styled-components/native';
+import CalendarPicker from 'react-native-calendar-picker';
+import 'moment/locale/pt-br'; // Adicionado para definir o idioma para português
 
-const CalendarScreen = ({ navigation }) => {
-    return (
-        <Background>
-            <View style={styles.container}>
-                <Text>Calendario Screen</Text>
-            </View>
-        </Background>
-    );
+const Container = styled(View)`
+  flex: 1;
+  margin-top: 25%;
+`;
+
+const Imagem = styled(Image)`
+    width: 15px;
+    height: 26px;
+`;
+
+const StyledCalendarPicker = styled(CalendarPicker)`
+`;
+
+class CalendarScreen extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedStartDate: null,
+        };
+        this.onDateChange = this.onDateChange.bind(this);
+    }
+
+    onDateChange(date) {
+        this.setState({
+            selectedStartDate: date,
+        });
+    }
+
+    render() {
+        const { selectedStartDate } = this.state;
+        const startDate = selectedStartDate ? selectedStartDate.toString() : '';
+
+        return (
+            <Background>
+                <Container>
+                    <StyledCalendarPicker
+                        onDateChange={this.onDateChange}
+                        weekdays={['D', 'S', 'T', 'Q', 'Q', 'S', 'S']}
+                        months={[
+                            'Janeiro',
+                            'Fevereiro',
+                            'Março',
+                            'Abril',
+                            'Maio',
+                            'Junho',
+                            'Julho',
+                            'Agosto',
+                            'Setembro',
+                            'Outubro',
+                            'Novembro',
+                            'Dezembro',
+                        ]}
+                        textStyle={{
+                            fontSize: 18,
+                            color: 'white',
+                        }}
+                        selectedDayTextStyle={{
+                        }}
+                        customDatesStyles={[
+                            {
+                                date: new Date(),
+                            },
+                        ]}
+                        todayTextStyle={[{
+                            color: 'black',
+                        }]}
+                        monthTitleStyle	={{
+                            fontSize: 18,
+                            fontFamily: 'Inter Bold'
+                        }}
+                        yearTitleStyle	={{
+                            fontSize: 18,
+                            fontFamily: 'Inter Bold' 
+                        }}
+                        selectedDayColor="#9F238E"
+                        selectedDayTextColor="white"
+                        nextComponent={<Imagem source={require('@assets/icons/proximo.png')} />} 
+                        previousComponent={<Imagem source={require('@assets/icons/anterior.png')} />} 
+                        dayLabelsWrapper={{ borderTopWidth: 0, borderBottomWidth: 0 }}
+                        selectMonthTitle = "Selecionar Mês em "
+                        selectYearTitle = "Selecionar Ano"
+                        headerWrapperStyle={{
+                           padding: 20
+                        }}
+                    />
+                    <View>
+                        <Text>SELECTED DATE: {startDate}</Text>
+                    </View>
+                </Container>
+            </Background>
+        );
+    }
 }
 
 export default CalendarScreen;
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
-})
