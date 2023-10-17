cbe46be298f2e079dcba87d14b60a8268b7aa3ba
CarlaChicareli <20212014267@aluno.cefetmg.br>
2023-10-16 21:55:50 -0300

    Feito a confirmação de sair quando está logado

--- screens/ConfigScreen/index.js
+++ screens/ConfigScreen/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'; // Importe o useState
+import React, { useState } from 'react';
 import { ScrollView } from 'react-native';
 import styled from 'styled-components/native';
 import ConfigItem from '@components/ConfigComponets/ConfigItem';
@@ -6,64 +6,87 @@ import Barra from '@components/Barra';
 import LogOff from '@components/ConfigComponets/LogOff';
 import Background from '@components/Background';
 import Logged from '@components/ConfigComponets/Logged';
+import AlertModal from '@components/Modals/AlertModal';
 
 const ConfigScreen = ({ navigation }) => {
+  const [isLogged, setIsLogged] = useState(false);
+  const [isLogoutModalVisible, setIsLogoutModalVisible] = useState(false);
 
-  const ToNotificacoes = ({}) => {
+  const ToNotificacoes = () => {
     navigation.navigate('Notificacoes');
   };
 
-  const ToBackup = ({}) => {
+  const ToBackup = () => {
     navigation.navigate('Backup');
-  }
+  };
 
-  const ToFaleConosco = ({}) => {
+  const ToFaleConosco = () => {
     navigation.navigate('FaleConosco');
-  }
+  };
 
   const handleLogout = () => {
-    setIsLogged(false); // Define o estado para "false" ao fazer logout
+    if (isLogged) {
+      setIsLogoutModalVisible(true);
+    }
   };
-  
-  //Defina true para logado e false para deslogado
-  const [isLogged, setIsLogged] = useState(false);
+
+  const confirmLogout = () => {
+    setIsLogoutModalVisible(false);
+    setIsLogged(false);
+  };
+
+  const cancelLogout = () => {
+    setIsLogoutModalVisible(false);
+  };
 
   const margin = 30;
 
   return (
     <Background>
-        <ScrollView contentContainerStyle={{ paddingBottom: 60 }}>
-            <Container>
-            
-                {isLogged ? <Logged /> : <LogOff setIsLogged={setIsLogged}/> }
+      <ScrollView contentContainerStyle={{ paddingBottom: 60 }}>
+        <Container>
+          {isLogged ? <Logged /> : <LogOff setIsLogged={setIsLogged} />}
 
-                <Content>
-                    <Barra
-                      marginTop= {margin}
-                    />
-                </Content>
+          <Content>
+            <Barra marginTop={margin} />
+          </Content>
 
-                <ConfigItem label="Notificações" iconSource={require('../../assets/Config/notificacao.png')} onPress={ToNotificacoes} />
-                <ConfigItem label="Backup em nuvem" iconSource={require('../../assets/Config/backup.png')} onPress={ToBackup} />
-                <ConfigItem label="Fale conosco" iconSource={require('../../assets/Config/telefone.png')} onPress={ToFaleConosco} />
-                <ConfigItem label="Sair" iconSource={require('../../assets/Config/sair.png')} onPress={handleLogout} />
+          <ConfigItem label="Notificações" iconSource={require('../../assets/Config/notificacao.png')} onPress={ToNotificacoes} />
+          <ConfigItem label="Backup em nuvem" iconSource={require('../../assets/Config/backup.png')} onPress={ToBackup} />
+          <ConfigItem label="Fale conosco" iconSource={require('../../assets/Config/telefone.png')} onPress={ToFaleConosco} />
 
-            </Container>
-        </ScrollView>
+          {isLogged && (
+            <ConfigItem
+              label="Sair"
+              iconSource={require('../../assets/Config/sair.png')}
+              onPress={handleLogout}
+            />
+          )}
+
+          <AlertModal
+            visible={isLogoutModalVisible}
+            content="Tem certeza que deseja sair?"
+            button1Text='SAIR'
+            button2Text='CANCELAR'
+            onRequestButton1={confirmLogout}
+            onRequestButton2={cancelLogout}
+            button1Color="#BD2E32"
+            onClose={() => setIsLogoutModalVisible(false)}
+          />
+        </Container>
+      </ScrollView>
     </Background>
   );
 };
 
 const Content = styled.View`
-    flex-grow: 1;
-    margin-bottom: 10%;
+  flex-grow: 1;
+  margin-bottom: 10%;
 `;
 const Container = styled.View`
   flex: 1;
   flex-grow: 1;
   padding-top: 10%;
 `;
-
-
 
 export default ConfigScreen;
