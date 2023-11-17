69cd9a1aadfff84e5250d8358f88d66a7df78f7f
CarlaChicareli <20212014267@aluno.cefetmg.br>
2023-11-17 12:55:05 -0300

    removi o favoritar do drawer de sonhos deletados
    --> removi o compartilhar de tds os sonhos

--- components/EndComponents/Header.js
+++ components/EndComponents/Header.js
@@ -3,19 +3,19 @@ import { Button } from '../Headers/styles';
 import MainHeader from '@components/Headers/MainHeader';
 import backIcon from '@assets/icons/Voltar.png'
 
-const DreamHeader = ({ navigation, favorited, deleted, toggleFavorite, toggleDelete}) => {
+const DreamHeader = ({ navigation, favorited, deleted, toggleFavorite, toggleDelete }) => {
     const favoritosIcon = favorited
-    ? require('@assets/icons/favoritado.png')
-    : require('@assets/icons/favoritos.png');
-    
-    const lixoIcon = deleted 
-    ? require('@assets/icons/restore.png')
-    : require('@assets/icons/lixo.png');
+        ? require('@assets/icons/favoritado.png')
+        : require('@assets/icons/favoritos.png');
+
+    const lixoIcon = deleted
+        ? require('@assets/icons/restore.png')
+        : require('@assets/icons/lixo.png');
 
     const compartilharIcon = require('@assets/icons/compartilhar.png');
 
     return (
-        
+
         <MainHeader
             left={
                 <Button
@@ -25,21 +25,17 @@ const DreamHeader = ({ navigation, favorited, deleted, toggleFavorite, toggleDel
             }
             right={
                 <>
-                    <Button
-                        source={favoritosIcon}
-                        onPress={toggleFavorite}
-                    />
-
-                    <Button
-                        source={compartilharIcon}
-                        onPress={() => {}}
-                    />
+                    {!deleted && (
+                        <Button
+                            source={favoritosIcon}
+                            onPress={toggleFavorite}
+                        />
+                    )}
 
                     <Button
                         source={lixoIcon}
                         onPress={toggleDelete}
                     />
-
                 </>
             }
         />
